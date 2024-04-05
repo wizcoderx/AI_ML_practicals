db252898b9cda424da2eb10d3883d6844e98a821
wizcoderx <wizcoderx@gmail.com>
2024-04-05 13:39:48 +0530

    day_2
    this contains some dictionary_iterations and other stuff

---
+++ dictionary_iterations.py
@@ -0,0 +1,9 @@
+#Create a list
+dict = ['cat','monkey','dog']
+
+for xyz, animal in enumerate(dict):
+    print('%d: %s' % (xyz + 1 ,animal))
+    #print('A%d: %s' % (xyz + 1 ,animal))
+    #print('#%d:{here the starting attribute will affect the the entier values in the dictionary/list}%s' % (xyz + 1 ,animal))
+    #print('z%d: %s' % (xyz + 1 ,animal))
+    #print('$%d: %s' % (xyz + 1 ,animal))
